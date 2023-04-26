2212783861718d88617f808670975ec3c6a8a93a
Aylen <lag9512mail.com>
2023-04-26 15:44:14 -0300

    Modify events: use mousedown and mousemove to allow animations

--- src/app.js
+++ src/app.js
@@ -1,25 +1,53 @@
-const draggables = document.querySelectorAll('.draggable');
-const pieceContainer = document.querySelectorAll('.pieceContainer');
-const regex = /(piece)/g;
+const draggables = document.querySelectorAll('.draggable'),
+      pieceContainer = document.querySelectorAll('.pieceContainer'),
+      secondContainer = document.querySelector('.secondContainer'),
+      regex = /(piece)/g;
 
-// Lista de contenedores para las piezas
+
+// Lista de divs arrastrables, a cada uno le agrego una clase dragging cuando empiece a arrastrarse
+// Cuando lo suelte se eliminará la clase 
 draggables.forEach((draggable) => {
-  draggable.addEventListener('dragstart', () => {
+  const initial = draggable.getBoundingClientRect();
+
+  draggable.addEventListener('dragstart', e => {
     draggable.classList.add('dragging')
+    console.log(e.dataTransfer.effectAllowed)
+    setTimeout(() => draggable.classList.add('invisible', 0))
   })
 
   draggable.addEventListener('dragend', () => {
+    const isContainer = Object.values(draggable.parentNode.classList).includes('pieceContainer')
+    console.log(isContainer)
     draggable.classList.remove('dragging')
+    draggable.classList.remove('invisible')
+      
+    })
+    
   })
 
-})
+
+// Para cada contenedor de piezas agrego un evento dragover
+// La función devuelve el elemento más cercano según la posición del mouse en ese momento
+// Guardo el nombre de la clase que coincida con "piece", hago lo mismo con el elemento arrastrado
+// Si los nombres de las clases coinciden, meto el arrastrado al contenedor y le saco el draggable
+
 
 pieceContainer.forEach((container) => {
   container.addEventListener('dragover', e => {
-    e.preventDefault()
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const closestDiv = document.elementFromPoint(x,y);
+    const closestPiece = Object.values(closestDiv.classList).find((el) => regex.test(el));
+   
     const currDraggable = document.querySelector('.dragging');
     const pieceNumber = Object.values(currDraggable.classList).find((el) => regex.test(el));
-    container.appendChild(currDraggable);
+
+    if(closestPiece === pieceNumber){
+      e.preventDefault();
+      container.appendChild(currDraggable);
+      currDraggable.draggable = false;
+    }
   })
 
 })
