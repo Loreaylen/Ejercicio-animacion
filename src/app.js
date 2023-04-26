b6d9fb2e0e78cff6153a5147e9c7e1ca2513ca39
Lorena Aylen <lag9512@gmail.com>
2023-04-26 01:22:27 -0300

    Add dragover. Implement condition for divs with matching classes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,28 @@
 const draggables = document.querySelectorAll('.draggable');
-const containers = document.querySelectorAll('container');
+const pieceContainer = document.querySelectorAll('.pieceContainer');
+const regex = /(piece)/g;
+
+// Lista de contenedores para las piezas
+draggables.forEach((draggable) => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+
+})
+
+pieceContainer.forEach((container) => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const currDraggable = document.querySelector('.dragging');
+    const pieceNumber = Object.values(currDraggable.classList).find((el) => regex.test(el));
+    container.appendChild(currDraggable);
+  })
+
+})
+
+
+
