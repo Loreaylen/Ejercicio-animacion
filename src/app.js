5b39867cbb97f9ceecbc7e1d29fd19bb01ad0ab3
Lorena Aylen <lag9512@gmail.com>
2023-04-30 00:50:44 -0300

    Modified puzzle structure. Need to adapt closestNode function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const draggables = document.querySelectorAll('.draggable'),
-  pieceContainer = document.querySelectorAll('.pieceContainer'),
+  pieceContainer = document.querySelectorAll('.pieceNode'),
   secondContainer = document.querySelector('.secondContainer'),
   regex = /(piece)/g;
 let mousePosition = { x: null, y: null },
@@ -10,6 +10,7 @@ let mousePosition = { x: null, y: null },
   resetTransition = false,
   transitionTime = 400;
 
+  console.log([...pieceContainer])
 // Función para buscar la clase con el número de la pieza
 const getPieceClass = (element) => {
   return Object.values(element.classList).find(el => regex.test(el))
@@ -23,14 +24,19 @@ const getPieceClass = (element) => {
 const getClosestContainer = (draggable) => {
   const listOfPieceContainers = [...pieceContainer];
   return listOfPieceContainers.reduce((closest, curr) => {
+    const draggableRect = draggable.getBoundingClientRect();
+    const containerRect = curr.getBoundingClientRect();
+    console.log('draggableRect', draggableRect)
+  console.log('containerRect', containerRect)
+  const containerCenterX = containerRect.left + containerRect.width / 2;
+  const containerCenterY = containerRect.top + containerRect.height / 2;
+  const distance = (draggableRect.left - containerCenterX) ** 2 + (draggableRect.top - containerCenterY) ** 2;
 
-const offset = draggable.offsetTop - curr.offsetTop - curr.offsetHeight 
-
-if (offset < 0 && offset > closest.offset) {
-  return {offset: offset, element: curr}
+if (distance < closest.distance) {
+  return {distance: distance, element: curr}
 }
 else return closest
-  }, {offset: Number.NEGATIVE_INFINITY }).element
+  }, {distance: Infinity }).element
 }
 
 window.addEventListener('mousemove', e => {
@@ -64,6 +70,7 @@ draggables.forEach((draggable) => {
     draggable.style.zIndex = '1000';
     draggable.classList.add('dragging');
     pieceNumberOfDraggable = getPieceClass(draggable)
+    
   })
 
   draggable.addEventListener('mouseup', e => {
@@ -78,3 +85,24 @@ draggables.forEach((draggable) => {
 
 //para el mousedown hago la misma comprobación de las clases (capaz sea mejor separarla en una función aparte)
 // Si coinciden, meto el draggable en su contenedor, sino, disparo la animación para volver a su posición original
+
+
+// const draggableOne = document.querySelector('#draggableOne')
+// const dragRect = draggableOne.getBoundingClientRect()
+// console.log(dragRect.top, dragRect.left)
+// console.log(draggableOne.offsetX, draggableOne.offsetY)
+
+
+// TODO:
+/*
+-Agregar todos los 'nodos' para las piezas y acomodarlos
+-Cambiar los nombres de las clases -> los cuadraditos tienen que tener la clase 'pieceNode' y tanto el nodo como la pieza tienen que tener el número en una clase,
+la pieza va a tener un id con el número (para el clip-path) --> buscar sugerencias para nombrar las clases
+- Ajustar la función que calcula el offset para que sólo devuelva la pieza más cercana si tiene un offset de 15x15 respecto al contenedor
+(de lo contrario se desencadena la animación para volver a la posición original)
+- Si el número de la pieza coincide con el del nodo, entonces appendChild a la sección (tal vez conviene meter los nodos dentro de una sección)
+
+Luego de resolver la funcionalidad del rompecabezas:
+- Reemplazar por imágenes las piezas
+- Desarrollar una función que cuente cuántos nodos están completos, si llega al total entonces se activa la animación final
+ */
